06e954bd94532dd083f2f5528e924de3f0f273ce
Sergio Infante <raulsergio9@gmail.com>
2012-06-22 15:17:14 -0500

    Initial code

---
+++ website/docswarehouse/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from docswarehouse.models import Instancia, Categoria, Facultad, Interesado, Resolucion
+
+admin.site.register(Instancia)
+admin.site.register(Categoria)
+admin.site.register(Facultad)
+admin.site.register(Interesado)
+admin.site.register(Resolucion)
---
+++ website/docswarehouse/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Instancia(models.Model):
+	nombre = models.CharField(max_length=255)
+
+	def __unicode__(self):
+		return self.nombre
+
+class Categoria(models.Model):
+	nombre = models.CharField(max_length=255)
+
+	def __unicode__(self):
+		return self.nombre
+
+class Facultad(models.Model):
+	nombre = models.CharField(max_length=255)
+
+	def __unicode__(self):
+		return self.nombre
+
+class Interesado(models.Model):
+	nombre = models.CharField(max_length=255)
+
+	def __unicode__(self):
+		return self.nombre
+
+class Resolucion(models.Model):
+	codigo_resolucion = models.CharField(max_length=13)
+	fecha_emision = models.DateField()
+	asunto = models.TextField()
+	instancia = models.ForeignKey(Instancia)
+	categoria = models.ForeignKey(Categoria)
+	facultad = models.ForeignKey(Facultad, null=True, blank=True)
+	interesado = models.ForeignKey(Interesado)
+
+	def __unicode__(self):
+		return self.codigo_resolucion
+
