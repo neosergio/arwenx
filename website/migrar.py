53a5400d51ca0dfbf7de201cb6a81c74ef284dba
Sergio Infante <rsinfante@gmail.com>
2012-10-16 00:07:42 -0500

    primera prueba de migracion

--- website/migrar.py
+++ website/migrar.py
@@ -10,10 +10,23 @@ csv_datos = os.path.join(PROJ_PATH, 'db/secretaria.csv')
 sys.path.append(DIR_PATH)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
-from docswarehouse.models import Instancia
+from docswarehouse.models import Resolucion
+from docswarehouse.models import Instancia, Categoria
+from docswarehouse.models import Facultad, Interesado
+from django.contrib.auth.models import User
 print "Empezando la migracion"
 
 lector_registros = csv.reader(open(csv_datos), delimiter=',', quotechar = '\'')
+usuario = User.objects.get(pk=1)
 for elemento in lector_registros:
-	if elemento[0].startswith('0'):
-		print elemento[0] + elemento[1]
+    if elemento[0].startswith('0'):
+        codigo_migrar = elemento[0]
+        fecha_migrar = elemento[1]
+        asunto_migrar = elemento[2]
+        interesado_migrar, ok = Interesado.objects.get_or_create(nombre=elemento[3], registro=usuario)
+        instancia_migrar, ok = Instancia.objects.get_or_create(nombre=elemento[4], registro=usuario)
+        categoria_migrar, ok = Categoria.objects.get_or_create(nombre=elemento[5], registro=usuario)
+        facultad_migrar, ok = Facultad.objects.get_or_create(nombre=elemento[6], registro=usuario)
+        Resolucion.objects.create(codigo_resolucion = codigo_migrar, fecha_emision = fecha_migrar,
+            asunto = asunto_migrar, instancia = instancia_migrar, categoria = categoria_migrar,
+            facultad = facultad_migrar, registro = usuario)
